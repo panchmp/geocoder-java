7254491c06259e745cfad179b1ac2d3815d64987
panchmp <panchmp@52ab988e-f1be-1f18-d91c-c61c6ef16ac2>
2011-12-19 02:19:00 +0000

    [maven-release-plugin]  copy for tag geocoder-java-0.9

---
+++ tags/geocoder-java-0.9/src/test/java/com/google/code/geocoder/MassGeocoderTest.java
@@ -0,0 +1,47 @@
+package com.google.code.geocoder;
+
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@Ignore
+public class MassGeocoderTest extends BaseGeocoderTest {
+
+    @Test
+    public void testGeocode() throws Exception {
+        GeocodeResponse geocoderResponse;
+
+        InputStream file = new FileInputStream("src/test/resources/ATM_partners.xls ");
+        try {
+            HSSFWorkbook wb = new HSSFWorkbook(file);
+            final HSSFSheet sheet = wb.getSheetAt(0);
+
+            for (int i = sheet.getLeftCol(); i < Math.min(2000, sheet.getLastRowNum()); i++) {
+                final HSSFRow row = sheet.getRow(i);
+
+                final String street = row.getCell(3).getStringCellValue();
+                final String city = row.getCell(1).getStringCellValue();
+                final String region = row.getCell(2).getStringCellValue();
+
+                String address = street + ", " + city;
+                if (!region.startsWith("м.")) {
+                    address += ", " + region + " обл.";
+                }
+
+                System.out.println("address = " + address);
+                geocoderResponse = geocoder.geocode(new GeocoderRequest(address, "uk"));
+                System.out.println("geocoderResponse = " + geocoderResponse);
+                Thread.sleep(1000);
+            }
+        } finally {
+            file.close();
+        }
+    }
+}
