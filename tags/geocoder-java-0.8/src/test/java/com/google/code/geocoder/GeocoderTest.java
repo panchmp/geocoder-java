93146de53af191a22bced929639a318ad24b24a4
panchmp <panchmp@52ab988e-f1be-1f18-d91c-c61c6ef16ac2>
2011-12-19 02:05:59 +0000

    [maven-release-plugin]  copy for tag geocoder-java-0.8

---
+++ tags/geocoder-java-0.8/src/test/java/com/google/code/geocoder/GeocoderTest.java
@@ -0,0 +1,57 @@
+package com.google.code.geocoder;
+
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.LatLng;
+import com.google.code.geocoder.model.LatLngBounds;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:panchmp@gmail.com">Michael Panchenko</a>
+ */
+public class GeocoderTest extends BaseGeocoderTest {
+    @Test
+    public void testURL() throws Exception {
+        String res;
+
+        res = geocoder.getURL(new GeocoderRequestBuilder().setAddress("Paris, France").setLanguage("fr").getGeocoderRequest());
+
+        assertEquals("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=Paris%2C+France&language=fr", res);
+
+        res = geocoder.getURL(new GeocoderRequestBuilder().setAddress("Paris, France").setLanguage("en").setRegion("fr").getGeocoderRequest());
+        assertEquals("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=Paris%2C+France&language=en&region=fr", res);
+
+
+        res = geocoder.getURL(new GeocoderRequestBuilder().setAddress("Winnetka").setBounds(new LatLngBounds(new LatLng("34.172684", "-118.604794"), new LatLng("34.236144", "-118.500938"))).getGeocoderRequest());
+        assertEquals("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=Winnetka&bounds=34.172684%2C-118.604794%7C34.236144%2C-118.500938", res);
+    }
+
+    @Test
+    public void testGeocode() throws Exception {
+        GeocodeResponse geocoderResponse;
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Paris, France").setLanguage("fr").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Paris, France").setLanguage("en").setRegion("fr").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Paris, France").setLanguage("en").setRegion("fr").setBounds(new LatLngBounds(new LatLng("49.01", "2.51"), new LatLng("48.02", "2.03"))).getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Germany, Munich").setLanguage("en").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Бровари, вул. Київська 17").setLanguage("uk").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Дніпропетровськ, ул. Артема 28").setLanguage("uk").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("中国上海安化路").setLanguage("zn").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+
+        geocoderResponse = geocoder.geocode(new GeocoderRequestBuilder().setAddress("Odessa, TX").getGeocoderRequest());
+        assertNotNull(geocoderResponse);
+    }
+}
+
---
+++ tags/geocoder-java-0.8/src/test/java/com/google/code/geocoder/ProxyGeocoderTest.java
@@ -0,0 +1,16 @@
+package com.google.code.geocoder;
+
+import org.junit.BeforeClass;
+
+/**
+ * @author <a href="mailto:panchmp@gmail.com">Michael Panchenko</a>
+ */
+public class ProxyGeocoderTest extends GeocoderTest {
+    @BeforeClass
+    public static void setUp() {
+        final AdvancedGeoCoder advancedGeoCoder = new AdvancedGeoCoder();
+        advancedGeoCoder.getHttpClient().getHostConfiguration().setProxy("41.190.16.17", 8080);
+        geocoder = advancedGeoCoder;
+    }
+}
+
